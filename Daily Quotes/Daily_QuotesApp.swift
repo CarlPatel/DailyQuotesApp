6fd2d2e12b8af4f8146e49a87fd8d685189d093c
CarlPatel <carlpatel1217@gmail.com>
2025-07-30 15:26:44 -0700

    Initial Commit

---
+++ Daily Quotes/Daily_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_QuotesApp.swift
+//  Daily Quotes
+//
+//  Created by Carl Patel on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Daily_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
